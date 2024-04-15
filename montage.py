ec6089991f76b5fc867fd0b99d9e0e128ae82d81
Phillip Pollard <phil@crescendo.net>
2024-04-15 16:59:58 -0400

    Start of a UI
    * A simple UI, setup to select the 2 directories and 1 file required
      to start things.
    
    * On successful selection of a file and 2 directories, it exits.
    
    * Pylint ignores on PyQT warnings on module names. PyQT lazily
      imports names at runtime. So this warning is not a concern.

--- montage.py
+++ montage.py
@@ -1,6 +1,8 @@
 """ Draw an Image Montage """
 
-from PyQt6.QtWidgets import (
+import os.path
+
+from PyQt6.QtWidgets import (  # pylint: disable=no-name-in-module
     QApplication,
     QFileDialog,
     QPushButton,
@@ -8,16 +10,26 @@ from PyQt6.QtWidgets import (
     QWidget,
     QLabel
 )
-from PyQt6.QtCore import pyqtSlot
+from PyQt6.QtCore import pyqtSlot  # pylint: disable=no-name-in-module
 
 class Window(QWidget):
+    """ Main app class """
+
     def __init__(self):
         super().__init__()
+        self.draw_ui()
+
+    def draw_ui(self):
+        """ draw the main app window """
         self.setWindowTitle("Montage Builder")
 
-        self.imagedir = QPushButton("Image Directory")
-        self.imagedir.clicked.connect(self.get_dir)
         self.imagedir_label = QLabel("Pick a Directory")
+        self.imagedir = QPushButton("Image Directory")
+        self.imagedir.clicked.connect(lambda: self.get_dir(self.imagedir_label))
+
+        self.cache_label = QLabel("Pick a Cache Directory")
+        self.cache = QPushButton("Cache Directory")
+        self.cache.clicked.connect(lambda: self.get_dir(self.cache_label))
 
         self.goal = QPushButton("Goal Image")
         self.goal.clicked.connect(self.get_file)
@@ -26,21 +38,34 @@ class Window(QWidget):
         layout = QGridLayout()
         layout.addWidget(self.imagedir, 0, 0)
         layout.addWidget(self.imagedir_label, 0, 1)
-        layout.addWidget(self.goal, 1, 0)
-        layout.addWidget(self.goal_label, 1, 1)
+        layout.addWidget(self.cache, 1, 0)
+        layout.addWidget(self.cache_label, 1, 1)
+        layout.addWidget(self.goal, 2, 0)
+        layout.addWidget(self.goal_label, 2, 1)
         self.setLayout(layout)
 
+    def check_status(self):
+        """ Check if we have selected valid entries and can proceed to processing """
+        if os.path.exists(self.imagedir_label.text()) and \
+           os.path.exists(self.cache_label.text()) and \
+           os.path.isfile(self.goal_label.text()):
+            print("Exited...")
+            app.quit()
+
     @pyqtSlot()
-    def get_dir(self):
+    def get_dir(self, label):
+        """ Dialog to select a directory """
         fname = QFileDialog.getExistingDirectory(
             self,
             "Select an input directory",
             "${HOME}"
         )
-        self.imagedir_label.setText(fname)
+        label.setText(fname)
+        self.check_status()
 
     @pyqtSlot()
     def get_file(self):
+        """ Dialog to select the goal file """
         fname = QFileDialog.getOpenFileName(
             self,
             "Open File",
@@ -48,6 +73,7 @@ class Window(QWidget):
             "All Files (*);; Python Files (*.py);; PNG Files (*.png)",
         )
         self.goal_label.setText(fname[0])
+        self.check_status()
 
 
 app = QApplication([])
