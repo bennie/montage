4e0edb5c44ab2f46e1e1598f0369bfa1ce071476
Phillip Pollard <phil@crescendo.net>
2024-05-04 21:01:08 -0400

    Now processing thumbnails

--- montage.py
+++ montage.py
@@ -1,5 +1,6 @@
 """ Draw an Image Montage """
 
+import hashlib
 import os.path
 
 from pathlib import Path
@@ -19,35 +20,44 @@ from PyQt6.QtCore import (  # pylint: disable=no-name-in-module
     QThread,
     Qt
 )
+from wand.image import Image
 
-from lib.montage import is_image
+import yaml
 
-class Worker(QObject):  # pylint: disable=too-few-public-methods
+from lib.montage import is_image, makethumb
+
+
+class Worker(QObject):
     """ Background worker for image processing """
     finished = pyqtSignal()
     progress = pyqtSignal(str)
 
-    def __init__(self, imagedir):
+    def __init__(self, config):
         super().__init__()
-        self.imagedir = imagedir
+        self.config = config
 
-    def run(self):
+    def scan_for_files(self):
         """Long-running task."""
-        for path in Path(self.imagedir).rglob('*'):
+        for path in Path(self.config['imagedir']).rglob('*'):
             if not path.is_file():
                 continue
             if not is_image(path.name):
                 continue
-
             self.progress.emit(str(path))
-
-
-            #md5 = hashlib.md5(str(path).encode()).hexdigest()
-            #outfile = os.path.join(config['thumbdir'], f"{md5}.{config['thumbnail_type']}")
-
-            #makethumb(path, outfile, height_ratio, pref_width, pref_height)
-
         self.finished.emit()
+
+    def make_thumbnails(self):
+        """Long-running task."""
+        for file in self.config['files']:
+            path = Path(file)
+            md5 = hashlib.md5(str(path).encode()).hexdigest()
+            outfile = os.path.join(self.config['thumbdir'],
+                                   f"{md5}.{self.config['thumbnail_type']}")
+            makethumb(path, outfile, self.config['height_ratio'],
+                      self.config['pref_width'], self.config['pref_height'])
+            self.progress.emit(md5)
+        self.finished.emit()
+
 
 class Window(QWidget):  # pylint: disable=too-many-instance-attributes
     """ Main app class """
@@ -57,6 +67,9 @@ class Window(QWidget):  # pylint: disable=too-many-instance-attributes
         self.files = []
         self.thread = None
         self.worker = None
+
+        with open('config.yaml', encoding="utf-8") as f:
+            self.config = yaml.safe_load(f)
 
         self.draw_ui()
 
@@ -97,22 +110,37 @@ class Window(QWidget):  # pylint: disable=too-many-instance-attributes
         self.setLayout(layout)
 
     def check_status(self):
-        """ Check if we have selected valid entries and can proceed to processing """
+        """ Check if we have picked the directories and can start processing
+
+        If we are good, calculate default sizes and write data into the config.
+        Then start the image scan.
+        """
         if os.path.exists(self.imagedir_label.text()) and \
            os.path.exists(self.cache_label.text()) and \
            os.path.isfile(self.goal_label.text()):
-            self.run_long_task(self.imagedir_label.text())
 
-    def run_long_task(self, imagedir):
-        """ A long running task to test """
+            self.config['imgdir'] = self.imagedir_label.text()
+            self.config['thumbdir'] = self.cache_label.text()
+            self.config['goal'] = self.goal_label.text()
+
+            with Image(filename=self.config['goal']) as img:
+                self.config['height_ratio'] = img.height / img.width
+                self.config['pref_width'] = self.config['default_size']
+                self.config['pref_height'] = int(
+                    self.config['default_size'] * self.config['height_ratio'])
+
+            self.scan_for_files()
+
+    def scan_for_files(self):
+        """ Crawl the file system to determine a list of files to examine """
         self.thread = QThread()
-        self.worker = Worker(imagedir)
+        self.worker = Worker(self.config)
         self.worker.moveToThread(self.thread)
-        self.thread.started.connect(self.worker.run)
+        self.thread.started.connect(self.worker.scan_for_files)
         self.worker.finished.connect(self.thread.quit)
         self.worker.finished.connect(self.worker.deleteLater)
         self.thread.finished.connect(self.thread.deleteLater)
-        self.worker.progress.connect(self.report_progress)
+        self.worker.progress.connect(self.scan_for_files_progress)
         self.thread.start()
 
         self.imagedir.setEnabled(False)
@@ -121,18 +149,55 @@ class Window(QWidget):  # pylint: disable=too-many-instance-attributes
         self.progress_bar.setRange(0,0)
         self.progress_label.setText("Finding files... ")
 
-        self.thread.finished.connect(lambda: self.diediedie())  # pylint: disable=unnecessary-lambda
+        self.thread.finished.connect(lambda: self.scan_for_files_done())  # pylint: disable=unnecessary-lambda
 
-    def diediedie(self):
-        """ Go Bye-Bye """
-        self.progress_bar.setRange(0,1)
-        self.progress_label.setText(f"{len(self.files)} files found")
-        self.status_text.setText("")
-
-    def report_progress(self, n):
+    def scan_for_files_progress(self, n):
         """ Update progress; stub for now """
         self.files.append(n)
         self.status_text.setText(f"{n[-25:]:25}")
+
+    def scan_for_files_done(self):
+        """ We've found all the files, start processing """
+        self.progress_bar.setRange(0,len(self.files))
+        self.progress_label.setText(f"{len(self.files)} files found")
+        self.status_text.setText("")
+
+        self.cache_files()
+
+
+    def cache_files(self):
+        """ Go through the file-list and create thumnails in the cache """
+        self.config['files'] = self.files # This can't be good
+
+        self.thread = QThread()
+        self.worker = Worker(self.config)
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.make_thumbnails)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.worker.progress.connect(self.cache_files_progress)
+        self.thread.start()
+
+        self.imagedir.setEnabled(False)
+        self.cache.setEnabled(False)
+        self.goal.setEnabled(False)
+        self.progress_bar.setRange(0,len(self.files))
+        self.progress_label.setText("Caching images... ")
+
+        self.thread.finished.connect(lambda: self.cache_files_done())  # pylint: disable=unnecessary-lambda
+
+    def cache_files_progress(self, md5val):
+        """ Update progress; stub for now """
+        self.status_text.setText(f"{md5val}")
+        newval = self.progress_bar.value() + 1
+        self.progress_label.setText(f"{newval} / {len(self.files)} cached")
+        self.progress_bar.setValue(newval)
+
+    def cache_files_done(self):
+        """ We've found all the files, start processing """
+        self.progress_label.setText(f"{len(self.files)} files cached?")
+        self.status_text.setText("")
 
     @pyqtSlot()
     def get_dir(self, label):
